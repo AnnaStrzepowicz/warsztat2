72f76c63f0da7fb2e8143e08c6e4dbad30191898
AnnaStrzepowicz <anna.strzepowicz@gmail.com>
2019-07-14 15:52:21 +0200

    Add sql to create table. Add Usermodel.

---
+++ src/pl/coderslab/warsztat2/model/User.java
@@ -0,0 +1,55 @@
+package pl.coderslab.warsztat2.model;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+public class User {
+    private int id;
+    private String username;
+    private String email;
+    private String password;
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        setPassword(password);
+    }
+
+    public User() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = BCrypt.hashpw(password, BCrypt.gensalt());
+    }
+
+    public boolean isPasswordCorrect(String password){
+        return BCrypt.checkpw(password, getPassword());
+    }
+}
